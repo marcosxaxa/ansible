d5d53d810b06ab70d9149989f34a67c7f16db283
xtenaz <marcos@xaxas.net>
2019-10-13 11:00:38 -0300

    new script and inventory

--- inventory.sh
+++ inventory.sh
@@ -4,7 +4,7 @@
 
 
 for i in $(docker ps -q)
-do ip=$(docker inspect $i |grep '"IPAddress":' |cut -d ':' -f 2 |cut -d '"' -f 2 |head -n 1) ; group=$(docker inspect $i |grep 'Platform":' |cut -d ':' -f 2 |cut -d '"' -f 2 |head -n 1) ; host_ubuntu=$(docker inspect $i |grep 'ubuntu' |cut -d ':' -f 2 |cut -d '"' -f 2 | cut -d '/' -f 2 | cut -d '_' -f 1 |head -n 1) ; host_centos=$(docker inspect $i |grep 'centos' |cut -d ':' -f 2 |cut -d '"' -f 2 | cut -d '/' -f 2 | cut -d '_' -f 1|head -n 1)
+do ip=$(docker inspect $i |grep '"IPAddress":' |cut -d ':' -f 2 |cut -d '"' -f 2 |tail -n 1) ; group=$(docker inspect $i |grep 'Platform":' |cut -d ':' -f 2 |cut -d '"' -f 2 |tail -n 1) ; host_ubuntu=$(docker inspect $i |grep 'ubuntu' |cut -d ':' -f 2 |cut -d '"' -f 2 | cut -d '/' -f 2 | cut -d '_' -f 1 |tail -n 1) ; host_centos=$(docker inspect $i |grep 'centos' |cut -d ':' -f 2 |cut -d '"' -f 2 | cut -d '/' -f 2 | cut -d '_' -f 1|tail -n 1)
 	if [ "$host_ubuntu" == "ubuntu" ]
 	then	
  	echo $host_ubuntu"-"$i ansible_host=$ip >> docker_inventory
